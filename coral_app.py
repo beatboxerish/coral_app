3c465ae5a5d3c6c9ce82d66d292fe7d8aea3c685
Ishan Nangia <ishannangia@Ishans-MacBook-Pro.local>
2023-11-04 16:38:52 +0530

    Add coral model

--- coral_app.py
+++ coral_app.py
@@ -1,17 +1,21 @@
 import streamlit as st
 from PIL import Image
 from io import BytesIO
-import base64
+from ultralytics import YOLO
+import cv2
+import numpy as np
 
-st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Remove background from your image")
+st.set_page_config(layout="wide", page_title="Coral Segmentation App")
+
+st.write("## Segment your Corals and Ref box")
 st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](https://github.com/tyler-simons/BackgroundRemoval) on GitHub. Special thanks to the [rembg library](https://github.com/danielgatis/rembg) :grin:"
+    ":dog: Upload an image with corals and center reference white tile. Final image can be downloaded from the sidebar."
 )
 st.sidebar.write("## Upload and download :gear:")
 
-MAX_FILE_SIZE = 5 * 1024 * 1024  # 5MB
+MAX_FILE_SIZE = 7 * 1024 * 1024  # 7MB
+
 
 # Download the fixed image
 def convert_image(img):
@@ -21,15 +25,54 @@ def convert_image(img):
     return byte_im
 
 
-def fix_image(upload):
+def get_segmentations(upload):
     image = Image.open(upload)
     col1.write("Original Image :camera:")
     col1.image(image)
 
-    col2.write("Fixed Image :wrench:")
+    col2.write("Segmentations :wrench:")
+    image = model_pipeline(image, model)
     col2.image(image)
     st.sidebar.markdown("\n")
-    st.sidebar.download_button("Download fixed image", convert_image(image), "fixed.png", "image/png")
+    st.sidebar.download_button("Download fixed image", convert_image(image), "segmentations.png", "image/png")
+
+
+def model_pipeline(image, model):
+    cv_image = np.array(image)
+    resized_image = resize_image(cv_image, (640, 490))
+    segmented_image = inference(resized_image, cv_image, model)
+    segmented_image = Image.fromarray(segmented_image)
+    return segmented_image
+
+
+def load_model():
+    model = YOLO('yolo_model.pt')
+    return model
+
+
+def resize_image(img, size):
+    resized_image = cv2.resize(img, size)
+    return resized_image
+
+
+def inference(resized_image, original_image, model):
+    result = model.predict(resized_image)
+    segmented_image = draw_bboxes_xyxyn(result[0].boxes.xyxyn, original_image)
+    return segmented_image
+
+
+def draw_bboxes_xyxyn(bboxes, img):
+    colors = [(150, 150, 150)]
+    drawn_img = img.copy()
+    for i, box in enumerate(bboxes):
+        x, y, x1, y1 = box
+        x, x1 = x*img.shape[1], x1*img.shape[1]
+        y, y1 = y*img.shape[0], y1*img.shape[0]
+        cv2.rectangle(drawn_img, (int(x), int(y)), (int(x1), int(y1)), colors[0], 10)
+    return drawn_img
+
+with st.spinner('Please wait while we load the model...'):
+    model = load_model()
 
 
 col1, col2 = st.columns(2)
@@ -37,8 +80,8 @@ my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpe
 
 if my_upload is not None:
     if my_upload.size > MAX_FILE_SIZE:
-        st.error("The uploaded file is too large. Please upload an image smaller than 5MB.")
+        st.error("The uploaded file is too large. Please upload an image smaller than 7MB.")
     else:
-        fix_image(upload=my_upload)
+        get_segmentations(upload=my_upload)
 else:
-    pass
+    st.error("Please upload a valid image.")
